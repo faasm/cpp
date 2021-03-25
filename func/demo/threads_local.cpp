3cb18e1ef0530008d80f994dbf44e62955e45fb4
Simon Shillaker <scs17@ic.ac.uk>
2021-03-25 17:57:48 +0000

    More interesting threading

--- func/demo/threads_local.cpp
+++ func/demo/threads_local.cpp
@@ -3,40 +3,58 @@
 
 /**
  * This function uses a thread that modifies a value on the stack
- * of the main thread, hence will only work when running in local
- * threading mode.
+ * of the main thread, hence will only work when all threads have access to the
+ * same shared memory.
  */
+
+struct threadArgs
+{
+    int idx;
+    int* ptr;
+};
 
 void* threadFunc(void* arg)
 {
-    // Write to the variable passed in
-    int* intArg = (int*)arg;
-    *intArg = 200;
+    // Write to the struct passed in
+    auto args = (threadArgs*)arg;
+    *args->ptr = 200 + args->idx;
     return nullptr;
 }
 
 int main()
 {
-    int x = 0;
+    // Spawn a couple of threads
+    int nThreads = 4;
+    int x[nThreads];
+    pthread_t threads[nThreads];
+    struct threadArgs args[nThreads];
 
-    // Spawn a thread
-    pthread_t t;
-    int ret = pthread_create(&t, NULL, threadFunc, &x);
-    if (ret != 0) {
-        fprintf(stderr, "Error creating thread (%i)\n", ret);
-        return 1;
+    for (int i = 0; i < nThreads; i++) {
+        args[i].idx = i;
+        args[i].ptr = &x[i];
+
+        int ret = pthread_create(&threads[i], NULL, threadFunc, &args[i]);
+        if (ret != 0) {
+            fprintf(stderr, "Error creating thread (%i)\n", ret);
+            return 1;
+        }
     }
 
-    // Join the thread
-    if (pthread_join(t, nullptr)) {
-        fprintf(stderr, "Error joining thread\n");
-        return 1;
-    }
+    // Join the threads
+    for (int i = 0; i < nThreads; i++) {
+        if (pthread_join(threads[i], nullptr)) {
+            fprintf(stderr, "Error joining thread\n");
+            return 1;
+        }
 
-    // Check stack variable
-    if (x != 200) {
-        printf("Thread invocation failed (expected 200 but got %i)\n", x);
-        return 1;
+        // Check stack variable
+        int expected = 200 + i;
+        if (x[i] != expected) {
+            printf("Thread invocation failed (expected %i but got %i)\n",
+                   expected,
+                   x[i]);
+            return 1;
+        }
     }
 
     return 0;
