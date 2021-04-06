7d2a9dad66c069ac6f4d2ecd61b8b90ddfc6f7f8
Carlos Segarra <carlossegarragonzalez@gmail.com>
2021-04-06 14:29:20 +0100

    Cartesian tests (#39)

--- func/mpi/mpi_cartesian.cpp
+++ func/mpi/mpi_cartesian.cpp
@@ -3,6 +3,7 @@
 #include <faasm/faasm.h>
 #include <mpi.h>
 #include <stdio.h>
+#include <vector>
 
 int main(int argc, char* argv[])
 {
@@ -14,13 +15,22 @@ int main(int argc, char* argv[])
     MPI_Comm_size(MPI_COMM_WORLD, &worldSize);
 
     // Prepare arguments
-    int ndims = 2;
-    int sideLength = static_cast<int>(std::floor(std::sqrt(worldSize)));
-    int nprocs = sideLength * sideLength;
-    int dims[2] = { sideLength, sideLength };
-    int periods[2] = { 0, 0 };
+    // Note that the grid size is always 5x1x1 because the tests are always
+    // ran with MPI_WORLD_SIZE = 5
+    const int ndims = 3;
+    int dims[ndims] = { 5, 1, 1 };
+    int periods[ndims] = { 0, 0, 0 };
     int reorder = 0;
     MPI_Comm cart;
+
+    // Expected arguments
+    std::vector<std::vector<int>> expectedCoords = {
+        { 0, 0, 0 }, { 1, 0, 0 }, { 2, 0, 0 }, { 3, 0, 0 }, { 4, 0, 0 },
+    };
+    std::vector<std::vector<int>> expectedShift = {
+        { 4, 1, 0, 0, 0, 0 }, { 0, 2, 1, 1, 1, 1 }, { 1, 3, 2, 2, 2, 2 },
+        { 2, 4, 3, 3, 3, 3 }, { 3, 0, 4, 4, 4, 4 },
+    };
 
     // Test MPI_Cart_create
     if (MPI_Cart_create(MPI_COMM_WORLD, ndims, dims, periods, reorder, &cart) !=
@@ -31,45 +41,58 @@ int main(int argc, char* argv[])
     printf("MPI_Cart_create succesful check.\n");
 
     // Test MPI_Cart_get
-    int coords[2];
-    int expected[2];
-    if (rank >= nprocs) {
-        expected[0] = expected[1] = MPI_UNDEFINED;
-    } else {
-        expected[0] = rank / sideLength;
-        expected[1] = rank % sideLength;
-    }
+    int coords[ndims];
     if (MPI_Cart_get(cart, ndims, dims, periods, coords) != MPI_SUCCESS) {
         printf("MPI_Cart_get failed!\n");
         return 1;
     }
 
     // Test integrity of results
-    if (!faasm::compareArrays<int>(coords, expected, 2)) {
+    if (!faasm::compareArrays<int>(
+          coords, expectedCoords[rank].data(), ndims)) {
         printf("Wrong cartesian coordinates.\n");
         return 1;
     }
     printf("MPI_Cart_get succesful check.\n");
 
-    // Now check that the original rank can be retrieved.
-    if (rank < nprocs) {
-        int nRank;
-        if (MPI_Cart_rank(cart, coords, &nRank) != MPI_SUCCESS) {
-            printf("MPI_Cart_rank failed!\n");
-            return 1;
-        }
-        if (rank != nRank) {
-            printf(
-              "Wrong rank from cartesian coordinates. Expected: %i Got: %i",
-              rank,
-              nRank);
-            return 1;
-        }
-        printf("MPI_Cart_rank succesful check.\n");
+    // Test MPI_Cart_rank
+    // Retrieve original rank from cartesian coordinates
+    int nRank;
+    if (MPI_Cart_rank(cart, coords, &nRank) != MPI_SUCCESS) {
+        printf("MPI_Cart_rank failed!\n");
+        return 1;
     }
+    if (rank != nRank) {
+        printf("Wrong rank from cartesian coordinates. Expected: %i Got: %i",
+               rank,
+               nRank);
+        return 1;
+    }
+    printf("MPI_Cart_rank succesful check.\n");
 
     printf("MPI Cartesian Topology Checks Succesful.\n");
     MPI_Finalize();
+
+    // Test MPI_Cart_shift
+    // Shift cartesian coordinates along first axis
+    int src, dst;
+    for (int axis = 0; axis < 3; axis++) {
+        if (MPI_Cart_shift(cart, axis, 1, &src, &dst) != MPI_SUCCESS) {
+            printf("MPI_Cart_shift failed!\n");
+            return 1;
+        }
+        if (src != expectedShift[rank][2 * axis] or
+            dst != expectedShift[rank][2 * axis + 1]) {
+            printf("Wrong (src, dst) pair in cart shift. Expected (%i, %i) got "
+                   "(%i, %i)",
+                   expectedShift[rank][2 * axis],
+                   expectedShift[rank][2 * axis + 1],
+                   src,
+                   dst);
+            return 1;
+        }
+    }
+    printf("MPI_Cart_shift succesful check.\n");
 
     return 0;
 }
