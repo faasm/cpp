cf3b0c46d8993e6492b6dfc67ab4b9c630e4cf6b
Simon Shillaker <mail@simonshillaker.com>
2021-04-09 13:31:46 +0200

    Memory stress test

--- func/omp/mem_stress.cpp
+++ func/omp/mem_stress.cpp
@@ -2,13 +2,14 @@
 #include <omp.h>
 #include <string>
 
-#define ITERATIONS 100000
+#define ITERATIONS 100
 #define N_THREADS 20
 
 int main(int argc, char** argv)
 {
     int nThreads = 0;
-    int summation = 0;
+    int totalLoops = 0;
+    int expectedLoops = 3 * ITERATIONS;
     if (argc == 1) {
         nThreads = 2;
     } else {
@@ -19,9 +20,9 @@ int main(int argc, char** argv)
     uint8_t* ptrs[ITERATIONS];
 
 #pragma omp parallel for num_threads(nThreads) default(none)                   \
-  shared(ptrs, summation)
+  shared(ptrs, totalLoops)
     for (int i = 0; i < ITERATIONS; i++) {
-        summation++;
+        totalLoops++;
         size_t mallocSize = (i % 5) * 1024;
         auto mem = (uint8_t*)::malloc(mallocSize);
 
@@ -30,23 +31,31 @@ int main(int argc, char** argv)
     printf("Memory allocated by %i threads\n", nThreads);
 
 #pragma omp parallel for num_threads(nThreads) default(none)                   \
-  shared(ptrs, summation)
+  shared(ptrs, totalLoops)
     for (int i = 0; i < ITERATIONS; i++) {
-        summation++;
+        totalLoops++;
         ::free(ptrs[i]);
     }
     printf("Memory freed by %i threads\n", nThreads);
 
 #pragma omp parallel for num_threads(nThreads) default(none)                   \
-  shared(ptrs, summation)
+  shared(ptrs, totalLoops)
     for (int i = 0; i < ITERATIONS; i++) {
-        summation++;
+        totalLoops++;
         size_t mallocSize = (i % 6) * 1024;
         auto mem = (uint8_t*)::malloc(mallocSize);
 
         ptrs[i] = mem;
     }
 
-    printf("Successful multi-threaded memory test. %i loops.\n", summation);
-    return 0;
+    if (totalLoops != expectedLoops) {
+        printf("Memory stress test failed, expected %i loops but got %i",
+               expectedLoops,
+               totalLoops);
+        return 1;
+    } else {
+        printf("Successful multi-threaded memory test. %i loops.\n",
+               totalLoops);
+        return 0;
+    }
 }
