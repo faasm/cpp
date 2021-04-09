5c42f6fa2fef686320e1638b2f593e54183547d9
Simon Shillaker <mail@simonshillaker.com>
2021-04-09 13:16:22 +0200

    Updating mem stress function

--- func/omp/mem_stress.cpp
+++ func/omp/mem_stress.cpp
@@ -8,6 +8,7 @@
 int main(int argc, char** argv)
 {
     int nThreads = 0;
+    int summation = 0;
     if (argc == 1) {
         nThreads = 2;
     } else {
@@ -17,27 +18,35 @@ int main(int argc, char** argv)
     printf("Running mem stress test with %i threads\n", nThreads);
     uint8_t* ptrs[ITERATIONS];
 
-#pragma omp parallel for num_threads(nThreads) default(none) shared(ptrs)
+#pragma omp parallel for num_threads(nThreads) default(none)                   \
+  shared(ptrs, summation)
     for (int i = 0; i < ITERATIONS; i++) {
+        summation++;
         size_t mallocSize = (i % 5) * 1024;
-        auto mem = (uint8_t*)malloc(mallocSize);
+        auto mem = (uint8_t*)::malloc(mallocSize);
 
         ptrs[i] = mem;
     }
+    printf("Memory allocated by %i threads\n", nThreads);
 
-#pragma omp parallel for num_threads(nThreads) default(none) shared(ptrs)
+#pragma omp parallel for num_threads(nThreads) default(none)                   \
+  shared(ptrs, summation)
     for (int i = 0; i < ITERATIONS; i++) {
-        free(ptrs[i]);
+        summation++;
+        ::free(ptrs[i]);
     }
+    printf("Memory freed by %i threads\n", nThreads);
 
-#pragma omp parallel for num_threads(nThreads) default(none) shared(ptrs)
+#pragma omp parallel for num_threads(nThreads) default(none)                   \
+  shared(ptrs, summation)
     for (int i = 0; i < ITERATIONS; i++) {
+        summation++;
         size_t mallocSize = (i % 6) * 1024;
-        auto mem = (uint8_t*)malloc(mallocSize);
+        auto mem = (uint8_t*)::malloc(mallocSize);
 
         ptrs[i] = mem;
     }
 
-    printf("Successful multi-threaded memory test\n");
+    printf("Successful multi-threaded memory test. %i loops.\n", summation);
     return 0;
 }
