72f20ffe049e8639e74ffc172f130a8ae26066f6
Carlos Segarra <carlos@carlossegarra.com>
2021-12-17 08:57:33 +0000

    boilerplate for alltoalv

---
+++ func/mpi/mpi_alltoallv.cpp
@@ -0,0 +1,66 @@
+#include <faasm/compare.h>
+#include <faasm/faasm.h>
+#include <mpi.h>
+#include <stdio.h>
+#include <vector>
+
+int main(int argc, char* argv[])
+{
+    MPI_Init(NULL, NULL);
+
+    int rank, worldSize;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &worldSize);
+
+    int chunkSize = 2;
+    int fullSize = worldSize * chunkSize;
+
+    // Sendcount and send displacmements
+    std::vector<int> sendCount(worldSize);
+    std::vector<int> sdisps(worldSize);
+    for (int i = 0, int acc = 0; i < worldSize; i++) {
+        sendCount.at(i) = i + 1;
+        sdisps.at(i) = acc;
+        acc += i + 1;
+    }
+
+    // Recvcount and recv displacements
+    std::vector<int> recvCount(worldSize, rank + 1);
+    std::vector<int> rdisps(worldSize);
+    for (int i = 0, int acc = 0; i < worldSize; i++) {
+        rdisps.at(i)
+    }
+
+    // Arrays for sending and receiving
+    int* sendBuf = new int[fullSize];
+    int* expected = new int[fullSize];
+    int* actual = new int[fullSize];
+
+    // Populate data
+    for (int i = 0; i < fullSize; i++) {
+        // Send buffer from this rank
+        sendBuf[i] = (rank * 10) + i;
+
+        // Work out which rank this chunk of the expectation will come from
+        int rankOffset = (rank * chunkSize) + (i % chunkSize);
+        int recvRank = i / chunkSize;
+        expected[i] = (recvRank * 10) + rankOffset;
+    }
+
+    MPI_Alltoall(
+      sendBuf, chunkSize, MPI_INT, actual, chunkSize, MPI_INT, MPI_COMM_WORLD);
+
+    if (!faasm::compareArrays<int>(actual, expected, fullSize)) {
+        return 1;
+    }
+
+    printf("Rank %i: alltoall as expected\n", rank);
+
+    delete[] sendBuf;
+    delete[] actual;
+    delete[] expected;
+
+    MPI_Finalize();
+
+    return 0;
+}
