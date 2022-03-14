375808774df0a4a3a56dd68ea0ca809afda59602
Simon Shillaker <mail@simonshillaker.com>
2022-03-14 11:47:11 +0000

    Increment func to use counter

--- func/demo/increment.cpp
+++ func/demo/increment.cpp
@@ -5,19 +5,28 @@
 
 #include <stdio.h>
 
+#define KEY "incr_example"
+#define N_WORKERS 4
+
+int checkCount()
+{
+    faasm::incrementCounter(KEY);
+    return 0;
+}
+
 int main(int argc, char* argv[])
 {
-    const char* key = "incr_example";
+    faasm::initCounter(KEY);
 
-    faasm::incrementCounter(key);
-    faasmPushState(key);
+    // Dispatch chained calls
+    faasmChainBatch(checkCount, "", N_WORKERS);
 
-    int count = faasm::getCounter(key);
+    int count = faasm::getCounter(KEY);
 
-    // Return message
-    char output[13];
-    sprintf(output, "Counter: %03i", count);
-    faasmSetOutput((uint8_t*)output, 12);
+    if(count != N_WORKERS) {
+        printf("Count not as expected: %i != %i\n", count, N_WORKERS);
+        return 1;
+    }
 
     return 0;
 }
