e2a1076fb8aff100a53e073777db78fa56f0a696
Simon Shillaker <554768+Shillaker@users.noreply.github.com>
2022-03-18 14:28:59 +0100

    Update shared files example to use writeable shared files (#87)
    * Update shared files example
    
    * Formattinge

--- func/demo/shared_file.cpp
+++ func/demo/shared_file.cpp
@@ -1,38 +1,54 @@
-#include "faasm/faasm.h"
 #include "faasm/files.h"
-#include "faasm/input.h"
 
 #include <cstring>
 #include <errno.h>
 #include <stdio.h>
 #include <sys/stat.h>
 
-/**
- * Test reading a shared file
- */
+#define FILE_PATH "faasm://some_dir/test_file.txt"
+#define FILE_DIR "faasm://some_dir"
+
 int main(int argc, char* argv[])
 {
-    const char* inputStr = faasm::getStringInput("");
+    // Create dir if not exists
+    struct stat st = { 0 };
+    if (::stat(FILE_DIR, &st) == -1) {
+        ::mkdir(FILE_DIR, 0700);
+    }
 
-    // First stat the file to make sure it exists
-    struct stat64 res
-    {};
-    int statRes = stat64(inputStr, &res);
-    if (statRes != 0) {
-        printf("Failed to stat shared file: %s\n", strerror(errno));
+    // Delete file if it exists
+    FILE* check = fopen(FILE_PATH, "r");
+    if (check != nullptr) {
+        ::fclose(check);
+        printf("Shared file at %s exists\n", FILE_PATH);
+        ::remove(FILE_PATH);
+
+        // Confirm file doesn't exist
+        check = ::fopen(FILE_PATH, "r");
+        if (check != nullptr) {
+            printf("Shared file at %s exists after deletion\n", FILE_PATH);
+            return 1;
+        }
+    }
+
+    // Open the file for writing
+    FILE* wp = ::fopen(FILE_PATH, "w");
+    if (wp == nullptr) {
+        printf("Could not open shared file for writing at %s\n", FILE_PATH);
         return 1;
     }
 
-    // Open the file
-    char* content = faasm::readFileToString(inputStr);
-    if (content == nullptr) {
-        printf("Failed to open file at %s\n", inputStr);
+    // Write some data to the file
+    const char* contents = "This is some dummy content";
+    ::fprintf(wp, "%s", contents);
+    ::fclose(wp);
+
+    // Read in the data and check
+    const char* actual = faasm::readFileToString(FILE_PATH);
+    if (::strcmp(actual, contents) != 0) {
+        printf("File contents not as expected: %s != %s\n", actual, contents);
         return 1;
     }
-
-    // Write file content as output
-    long length = faasm::getFileLength(inputStr);
-    faasmSetOutput(BYTES(content), length);
 
     return 0;
 }
