2c76b8ec6ef4ea69103d189faa9fb983b1ad7dd9
FlowDrucka64 <flow.drucka64@gmx.at>
2022-10-12 10:26:17 +0200

    formatting changes

--- func/tf/minimal.cpp
+++ func/tf/minimal.cpp
@@ -8,57 +8,56 @@
 #include "tensorflow/lite/optional_debug_tools.h"
 
 #define INPUT_FILE_PATH "faasm://tflite/sample_model.tflite"
-#define TFLITE_MINIMAL_CHECK(x)                              \
-  if (!(x)) {                                                \
-    fprintf(stderr, "Error at %s:%d\n", __FILE__, __LINE__); \
-    exit(1);                                                 \
-  }
+#define TFLITE_MINIMAL_CHECK(x)                                                \
+    if (!(x)) {                                                                \
+        fprintf(stderr, "Error at %s:%d\n", __FILE__, __LINE__);               \
+        exit(1);                                                               \
+    }
 
 /**
-* This is an example that is minimal to read a model
-* from disk to check if the lib was compiled successfully.
-*
-* Example inspired from:
-* https://github.com/tensorflow/tensorflow/blob/master/tensorflow/lite/examples/minimal/minimal.cc
-*/
+ * This is an example that is minimal to read a model
+ * from disk to check if the lib was compiled successfully.
+ *
+ * Example inspired from:
+ * https://github.com/tensorflow/tensorflow/blob/master/tensorflow/lite/examples/minimal/minimal.cc
+ */
 
-int main() 
+int main()
 {
-   // Load model from file
-  std::unique_ptr<tflite::FlatBufferModel> model =
+    // Load model from file
+    std::unique_ptr<tflite::FlatBufferModel> model =
       tflite::FlatBufferModel::BuildFromFile(INPUT_FILE_PATH);
-  TFLITE_MINIMAL_CHECK(model != nullptr);
+    TFLITE_MINIMAL_CHECK(model != nullptr);
 
+    // Build the interpreter with the InterpreterBuilder.
+    // Note: all Interpreters should be built with the InterpreterBuilder,
+    // which allocates memory for the Interpreter and does various set up
+    // tasks so that the Interpreter can read the provided model.
+    tflite::ops::builtin::BuiltinOpResolver resolver;
+    tflite::InterpreterBuilder builder(*model, resolver);
+    std::unique_ptr<tflite::Interpreter> interpreter;
+    builder(&interpreter);
+    TFLITE_MINIMAL_CHECK(interpreter != nullptr);
 
-  // Build the interpreter with the InterpreterBuilder.
-  // Note: all Interpreters should be built with the InterpreterBuilder,
-  // which allocates memory for the Interpreter and does various set up
-  // tasks so that the Interpreter can read the provided model.
-  tflite::ops::builtin::BuiltinOpResolver resolver;
-  tflite::InterpreterBuilder builder(*model, resolver);
-  std::unique_ptr<tflite::Interpreter> interpreter;
-  builder(&interpreter);
-  TFLITE_MINIMAL_CHECK(interpreter != nullptr);
+    // Allocate tensor buffers.
+    TFLITE_MINIMAL_CHECK(interpreter->AllocateTensors() == kTfLiteOk);
+    printf("=== Pre-invoke Interpreter State ===\n");
+    tflite::PrintInterpreterState(interpreter.get());
 
-  // Allocate tensor buffers.
-  TFLITE_MINIMAL_CHECK(interpreter->AllocateTensors() == kTfLiteOk);
-  printf("=== Pre-invoke Interpreter State ===\n");
-  tflite::PrintInterpreterState(interpreter.get());
+    // Fill input buffers
+    // TODO(user): Insert code to fill input tensors.
+    // Note: The buffer of the input tensor with index `i` of type T can
+    // be accessed with `T* input = interpreter->typed_input_tensor<T>(i);`
 
-  // Fill input buffers
-  // TODO(user): Insert code to fill input tensors.
-  // Note: The buffer of the input tensor with index `i` of type T can
-  // be accessed with `T* input = interpreter->typed_input_tensor<T>(i);`
+    // Run inference
+    // TFLITE_MINIMAL_CHECK(interpreter->Invoke() == kTfLiteOk);
+    // printf("\n\n=== Post-invoke Interpreter State ===\n");
+    // tflite::PrintInterpreterState(interpreter.get());
 
-  // Run inference
-  //TFLITE_MINIMAL_CHECK(interpreter->Invoke() == kTfLiteOk);
-  //printf("\n\n=== Post-invoke Interpreter State ===\n");
-  //tflite::PrintInterpreterState(interpreter.get());
+    // Read output buffers
+    // TODO(user): Insert getting data out code.
+    // Note: The buffer of the output tensor with index `i` of type T can
+    // be accessed with `T* output = interpreter->typed_output_tensor<T>(i);`
 
-  // Read output buffers
-  // TODO(user): Insert getting data out code.
-  // Note: The buffer of the output tensor with index `i` of type T can
-  // be accessed with `T* output = interpreter->typed_output_tensor<T>(i);`
-
-  return 0;
+    return 0;
 }
