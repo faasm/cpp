f108005de5433ec1773e264d7e38fe19d3fc01e6
Carlos Segarra <carlos@carlossegarra.com>
2023-03-29 14:51:52 +0000

    mpi: revert debugging changes in mpi functions

--- func/mpi/mpi_isendrecv.cpp
+++ func/mpi/mpi_isendrecv.cpp
@@ -20,81 +20,53 @@ int main(int argc, char* argv[])
     int sendValue = rank;
     int recvValue = -1;
 
-    // Debug
-    if (rank == 0) {
-        MPI_Request sendRequest;
-        MPI_Isend(&sendValue, 1, MPI_INT, right, 0, MPI_COMM_WORLD, &sendRequest);
-    } else {
-        recvValue = -1;
-        MPI_Recv(
-          &recvValue, 1, MPI_INT, left, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    // Isend
 
-        // Check the received value is as expected
-        if (recvValue != left) {
-            printf("Rank %i - async not working properly (got %i expected %i)\n",
-                   rank,
-                   recvValue,
-                   left);
-            return 1;
-        }
-        printf("Rank %i - isend working properly\n", rank);
+    // Asynchronously send to the right
+    MPI_Request sendRequest;
+    MPI_Isend(&sendValue, 1, MPI_INT, right, 0, MPI_COMM_WORLD, &sendRequest);
+
+    // Receive from the left
+    recvValue = -1;
+    MPI_Recv(
+      &recvValue, 1, MPI_INT, left, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+    // Wait for both
+    MPI_Wait(&sendRequest, MPI_STATUS_IGNORE);
+
+    // Check the received value is as expected
+    if (recvValue != left) {
+        printf("Rank %i - async not working properly (got %i expected %i)\n",
+               rank,
+               recvValue,
+               left);
+        return 1;
     }
-
+    printf("Rank %i - isend working properly\n", rank);
     MPI_Barrier(MPI_COMM_WORLD);
 
-    int numIter = 1;
+    // Irecv
 
-    for (int i = 0; i < numIter; i++) {
-        printf("Starting iteration %i/%i\n", i + 1, numIter);
+    // Send to the right
+    MPI_Send(&sendValue, 1, MPI_INT, right, 0, MPI_COMM_WORLD);
 
-        // Isend
+    // Asynchronously receive from the left
+    recvValue = -1;
+    MPI_Request recvRequest;
+    MPI_Irecv(&recvValue, 1, MPI_INT, left, 0, MPI_COMM_WORLD, &recvRequest);
 
-        // Asynchronously send to the right
-        MPI_Request sendRequest;
-        MPI_Isend(&sendValue, 1, MPI_INT, right, 0, MPI_COMM_WORLD, &sendRequest);
+    // Wait
+    MPI_Wait(&recvRequest, MPI_STATUS_IGNORE);
 
-        // Receive from the left
-        recvValue = -1;
-        MPI_Recv(
-          &recvValue, 1, MPI_INT, left, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-
-        // Wait for both
-        MPI_Wait(&sendRequest, MPI_STATUS_IGNORE);
-
-        // Check the received value is as expected
-        if (recvValue != left) {
-            printf("Rank %i - async not working properly (got %i expected %i)\n",
-                   rank,
-                   recvValue,
-                   left);
-            return 1;
-        }
-        printf("Rank %i - isend working properly\n", rank);
-        MPI_Barrier(MPI_COMM_WORLD);
-
-        // Irecv
-
-        // Send to the right
-        MPI_Send(&sendValue, 1, MPI_INT, right, 0, MPI_COMM_WORLD);
-
-        // Asynchronously receive from the left
-        recvValue = -1;
-        MPI_Request recvRequest;
-        MPI_Irecv(&recvValue, 1, MPI_INT, left, 0, MPI_COMM_WORLD, &recvRequest);
-
-        // Wait
-        MPI_Wait(&recvRequest, MPI_STATUS_IGNORE);
-
-        // Check the received value is as expected
-        if (recvValue != left) {
-            printf("Rank %i - irecv not working properly (got %i expected %i)\n",
-                   rank,
-                   recvValue,
-                   left);
-            return 1;
-        }
-        printf("Rank %i - irecv working properly\n", rank);
+    // Check the received value is as expected
+    if (recvValue != left) {
+        printf("Rank %i - irecv not working properly (got %i expected %i)\n",
+               rank,
+               recvValue,
+               left);
+        return 1;
     }
+    printf("Rank %i - irecv working properly\n", rank);
 
     MPI_Finalize();
 
